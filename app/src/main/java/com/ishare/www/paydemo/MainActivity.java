d0a1e621f573e9baefe5f239ae3d95774adef18a
love_311 <13628180947@qq.com>
2016-11-28 15:04:59 +0800

    Initial commit

---
+++ app/src/main/java/com/ishare/www/paydemo/MainActivity.java
@@ -0,0 +1,114 @@
+package com.ishare.www.paydemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.ci123.service.pay.alipay.Alipay;
+import com.ci123.service.pay.weixin.WXPay;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button mButton1,mButton2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mButton1 = (Button) findViewById(R.id.btn1);
+        mButton2 = (Button) findViewById(R.id.btn2);
+        mButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                doAliPay("");
+            }
+        });
+        mButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                doWXPay("");
+            }
+        });
+    }
+    /**
+     * 微信支付
+     * @param pay_param 支付服务生成的支付参数
+     */
+    private void doWXPay(String pay_param) {
+        String wx_appid = "wx6b69bdbf2adca4f8";     //替换为自己的appid
+        WXPay.init(getApplicationContext(), wx_appid);      //要在支付前调用
+        WXPay.getInstance().doPay(pay_param, new WXPay.WXPayResultCallBack() {
+            @Override
+            public void onSuccess() {
+                Toast.makeText(getApplication(), "支付成功", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onError(int error_code) {
+                switch (error_code) {
+                    case WXPay.NO_OR_LOW_WX:
+                        Toast.makeText(getApplication(), "未安装微信或微信版本过低", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case WXPay.ERROR_PAY_PARAM:
+                        Toast.makeText(getApplication(), "参数错误", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case WXPay.ERROR_PAY:
+                        Toast.makeText(getApplication(), "支付失败", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+
+            @Override
+            public void onCancel() {
+                Toast.makeText(getApplication(), "支付取消", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    /**
+     * 支付宝支付
+     * @param pay_param 支付服务生成的支付参数
+     */
+    private void doAliPay(String pay_param) {
+        new Alipay(this, pay_param, new Alipay.AlipayResultCallBack() {
+            @Override
+            public void onSuccess() {
+                Toast.makeText(getApplication(), "支付成功", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onDealing() {
+                Toast.makeText(getApplication(), "支付处理中...", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onError(int error_code) {
+                switch (error_code) {
+                    case Alipay.ERROR_RESULT:
+                        Toast.makeText(getApplication(), "支付失败:支付结果解析错误", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case Alipay.ERROR_NETWORK:
+                        Toast.makeText(getApplication(), "支付失败:网络连接错误", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case Alipay.ERROR_PAY:
+                        Toast.makeText(getApplication(), "支付错误:支付码支付失败", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    default:
+                        Toast.makeText(getApplication(), "支付错误", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+
+            }
+
+            @Override
+            public void onCancel() {
+                Toast.makeText(getApplication(), "支付取消", Toast.LENGTH_SHORT).show();
+            }
+        }).doPay();
+    }
+}
